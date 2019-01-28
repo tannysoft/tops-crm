bc0a4ed39974a0deceb3343792f4aae53edc62bf
Supachok Phonchaiyasit <supachok.ponchiyasit@gmail.com>
2019-01-29 02:25:54 +0700

    commit

--- backend/src/middleware.php
+++ backend/src/middleware.php
@@ -2,3 +2,11 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+$corsOptions = array(
+    "origin" => "*",
+    "exposeHeaders" => array("Content-Type", "X-Requested-With", "X-authentication", "X-client"),
+    "allowMethods" => array('GET', 'POST', 'PUT', 'DELETE', 'OPTIONS')
+);
+$cors = new \CorsSlim\CorsSlim($corsOptions);
+
+$app->add($cors);
